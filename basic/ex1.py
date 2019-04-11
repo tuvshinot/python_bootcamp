73d032b5f66f8a79f82b3fce2429d09038fe4341
tuvshin <tuvshin.otgo@gmail.com>
2019-04-11 23:04:15 +0300

    day2 little basic review

---
+++ basic/ex1.py
@@ -0,0 +1,13 @@
+builder = '*'
+for i in range(0, 10):
+    print(builder)
+    builder += '*'
+
+y = 0
+builder = '*'
+while y < 10:
+    print(builder)
+    builder += '*'
+    y += 1
+
+print(f"Here is the value : {y}")
---
+++ basic/guess.py
@@ -0,0 +1,36 @@
+from random import randint
+
+while True:
+    print("Guessing game You have 3 lives to guess!")
+    live = 3
+    com_value = randint(1, 10)
+    print(str(com_value))
+    lost = False
+    won = False
+    for i in range(3):
+        userGuess = input("Guess a Number between 1 to 10 : ")
+        userGuess = int(userGuess)
+        if userGuess == com_value:
+            won = True
+            break
+        live -= 1
+        if userGuess < com_value:
+            print(f'{userGuess} is lower, lives left : {live}')
+        if userGuess > com_value:
+            print(f'{userGuess} is higher, lives left : {live}')
+        if live < 1:
+            lost = True
+            break
+
+    if lost:
+        inp = input("You Lost :(! << try again?(y/n)")
+        if inp == 'y':
+            continue
+        else:
+            break
+    if won:
+        inp = input("You Won :)! << try again?(y/n)")
+        if inp == 'y':
+            continue
+        else:
+            break
